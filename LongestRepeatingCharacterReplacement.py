b5166d8039280dc5f425de0e1d42c58f44bc1cee
Madan <kmmr656@email.com>
2026-01-03 23:01:54 +0530

    longest repeating character replacement

---
+++ LongestRepeatingCharacterReplacement.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+s = "AABABBAAAANAA"
+k = 2
+count = defaultdict(int)
+
+left = 0
+max_freq = 0
+result = 0
+
+for right in range(len(s)):
+    count[s [right]] += 1
+    max_freq = max(max_freq,count[s[right]])
+
+    while (right - left +1) - max_freq > k:
+        count[s[left]] -= 1
+        left += 1
+    result = max(result,right-left+1)
+print(result)
+
