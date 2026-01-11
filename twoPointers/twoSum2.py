c42df079c548190bd2031768be15cf57f2199c5f
Madan <kmmr656@email.com>
2026-01-11 15:29:30 +0530

    twosum2 binary search approach

--- twoPointers/twoSum2.py
+++ twoPointers/twoSum2.py
@@ -1,5 +1,5 @@
 # 167. Two Sum II - Input Array Is Sorted
-
+# broute force approach
 def twoSum(numbers, target):
     '''
     time complexity: O(n^2)
@@ -12,8 +12,31 @@ def twoSum(numbers, target):
                 return [i+1,j+1]
         
         
-res=twoSum([2,7,11,15],17)
-print(res)
+        
+# binary search approach
+def twoSumBinary(numbers, target):
+    '''
+    time complexity: O(nlogn)
+    space complexity: O(1)
+    hits TLE on leetcode
+    '''
+    for i in range(len(numbers)):
+        left = i + 1
+        right = len(numbers)-1
+        differ = target - numbers[i]
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if numbers[mid] == differ:
+                return [i + 1, mid + 1]
+            elif numbers[mid] < differ:
+                left += 1
+            else:
+                right -= 1
+            
+res=twoSumBinary([2,7,11,15],13)
+print(res) #[1,3]
 
 
         
