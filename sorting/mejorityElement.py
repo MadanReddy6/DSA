816d1a8ede9153acdf3f8b0226b25e18aae19f09
Madan <kmmr656@email.com>
2026-01-26 17:06:10 +0530

    moore's voting algorithsm

--- sorting/mejorityElement.py
+++ sorting/mejorityElement.py
@@ -10,3 +10,25 @@ def majorityElement(nums):
                 return num
 res = majorityElement([3,2,3,2,2,2,2,2,2])          
 print(res)
+
+# moore's voting algorithm
+
+def majorityElement(nums):
+    count = 0
+    candidate = 0
+    
+    for  num in nums:
+        if count == 0:
+            candidate = num
+            count = 1
+        if num == candidate:
+            count += 1
+        else:
+            count -= 1
+    if nums.count(candidate) > len(nums) // 2:
+        return candidate
+    else:
+        return 'Majority element not exits' 
+            
+res = majorityElement([3,2,3,2,2,2,2,2,2])          
+print(res)
